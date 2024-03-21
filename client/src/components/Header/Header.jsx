d0bc009fe7bc970f325f6e554b365bb081ed7c90
Cristina Mateo <cris.mateo.99@gmail.com>
2024-03-21 12:31:43 +0100

    correcciones header

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,8 +3,10 @@ import React from "react";
 const Header = () => {
   return (
     <header>
+      <section>
       <img src="/assets/lilacE.png" alt="exchange-icon" />
       <h1>unit converter</h1>
+      </section>
     </header>
   );
 };
