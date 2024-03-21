d0a46b15a34d3dc8cee3c2506c4d76f8421f3d0a
Cristina Mateo <cris.mateo.99@gmail.com>
2024-03-21 12:15:22 +0100

    header y footer + styled

---
+++ client/src/components/Header/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header>
+      <img src="/assets/lilacE.png" alt="exchange-icon" />
+      <h1>unit converter</h1>
+    </header>
+  );
+};
+
+export default Header;
